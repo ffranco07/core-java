9ba70162e03b316d6d43e0ddc9c61b1c0344bcd6
Francisco Franco <franco_siete@yahoo.com>
2023-10-12 12:40:32 -0700

    Added java 2D arrays module

---
+++ src/java/fundamentals/Arrays2D.java
@@ -0,0 +1,43 @@
+public class Arrays2D {
+	public static String columnToStr(int[] column) {
+		StringBuilder builder = new StringBuilder();
+		builder.append("{");
+		for (int k = 0; k < column.length; k++) {
+			builder.append(column[k]);
+			if (k < (column.length - 1)) {
+				builder.append(", ");
+			}
+		}
+		builder.append("}");
+		return builder.toString();
+	}
+	
+	public static void main(String args[]) {
+		int[][] arr2d={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+		System.out.println("arr2d:" + arr2d);
+		System.out.println("arr2d length:" + arr2d.length);
+		System.out.println("arr2d[0]:" + columnToStr(arr2d[0]));
+		System.out.println("arr2d[0] length:" + arr2d[0].length);
+		System.out.println("arr2d[1]:" + columnToStr(arr2d[1]));
+		System.out.println("arr2d[1] length:" + arr2d[1].length);
+		System.out.println("arr2d[2]:" + columnToStr(arr2d[2]));
+		System.out.println("arr2d[2] length:" + arr2d[2].length);
+		System.out.println("arr2d[0][3]:" + arr2d[0][3]);
+
+		int rows = arr2d.length;
+		System.out.println("rows:" + rows);
+		int columns = arr2d[0].length;
+		System.out.println("columns" + columns);
+		int i, j;
+		StringBuilder builder = new StringBuilder();
+		for (i = 0; i < rows; i++) {
+			for (j = 0; j < columns; j++) {
+				builder.append(arr2d[i][j] + " ");
+			}
+			if (j != (columns - 1)) {
+				builder.append("\n");
+			}
+		}
+		System.out.println(builder.toString());
+	}
+}
