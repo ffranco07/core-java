300dc89e901f823370f1226e455fb5df1166117d
Francisco Franco <franco_siete@yahoo.com>
2023-10-17 12:15:43 -0700

    Updated InsertionSort comments

--- src/java/algorithms/InsertionSort.java
+++ src/java/algorithms/InsertionSort.java
@@ -5,7 +5,7 @@
  *
  * A comparison sort algorithm in which each iteration
  * removes an element from the input array and inserts it
- * into the correct position in the array.
+ * into the correct position in the array. In place algorithm.
  */
 
 public class InsertionSort {
@@ -18,22 +18,26 @@ public class InsertionSort {
 	/**
 	 * @param myArray
 	 */
-	private void printArray(boolean isOriginal, int[] myArray) {
-		if (isOriginal) {
-			System.out.print("Orig: ");
-		} else {
-			System.out.print(count + ") ");
+	private void printArray(String tag, int[] myArray) {
+		StringBuilder builder = new StringBuilder();
+
+		if (tag.equals("Step")) {
+			builder.append("Step " + count + ": ");
+			count++;
+		}
+		else {
+			builder.append(tag + ": ");
 		}
 		for (int l = 0; l < myArray.length; l++) {
-			System.out.print(myArray[l] + " ");
+			builder.append(myArray[l] + " ");
 		}
-		System.out.println("");
-		if (!isOriginal)
-			count++;
+		System.out.println(builder.toString());
 	}
 
 	/**
+	 * Best Time Complexity: O(n)
 	 * Worst Time Complexity: O(n^2)
+	 * Auxiliary Space: O(1)
 	 * 
 	 * ALWAYS START ITERATION AT 1
 	 * TO LOOK AT PREVIOUS ELEMENT
@@ -45,18 +49,25 @@ public class InsertionSort {
 		for (int i = 1; i < myArray.length; i++) {
 			value = myArray[i];
 			j = i - 1;
-			// Move ALL elements of arr[0..i-1], that are
+			// Shift right ALL elements of arr[0..i-1], that are
 			// greater than value, to one position ahead
 			// of their current position.
-			// while loop ends with negative j value
+			//
+			// The while loop ends with negative j value
+			// hence add 1 to value
 			while (j >=0 && myArray[j] > value) {
 				myArray[j+1] = myArray[j];
 				// Decrement j index
-				j = j-1;
+				j--;
 			}
 			// Set decremented j+1 element to value
 			myArray[j+1] = value;
-			printArray(false, myArray);
+			if (i == myArray.length - 1) {
+				printArray("Sorted", myArray);
+			}
+			else {
+				printArray("Step", myArray);
+			}
 		}
 		return myArray;
 	}
@@ -67,7 +78,7 @@ public class InsertionSort {
 	public static void main(String[] args) {
 		int[] myArray = { 12, 11, 13, 5, 6 };
 		InsertionSort is = new InsertionSort();
-		is.printArray(true, myArray);
+		is.printArray("Orig", myArray);
 		is.sortArray(myArray);
 	}
 }
