4fe863a9d49e6b6cd9ec251cbd57904a78b61227
Francisco Franco <franco_siete@yahoo.com>
2023-11-03 05:26:14 -0700

    MOved modules

--- src/main/java/algorithms/two-pointers/ReverseString.java
+++ src/main/java/algorithms/two-pointers/ReverseString.java
@@ -32,6 +32,19 @@ public class ReverseString {
 	}
 	
 	/**
+	 * Reverse input char array
+	 */
+	public void reverseString(char[] s) {
+		int i = 0;
+		for (int j = s.length - 1; j >= s.length / 2; j--) {
+			char temp = s[i];
+			s[i] = s[j];
+			s[j] = temp;
+			i++;
+		}
+	}
+	
+	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -40,5 +53,9 @@ public class ReverseString {
 		rs.printString("Original: " , myStr);
 		String reversedStr = rs.reverseString(myStr);
 		rs.printString("Reversed: ", reversedStr);
+		char[] s = {'h', 'e', 'l', 'l', 'o'};
+		System.out.println(s);
+		rs.reverseString(s);
+		System.out.println(s);
 	}
 }
