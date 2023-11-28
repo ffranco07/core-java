62f4452c0e9405ac3a5aeb860fb31df88b5801a5
Francisco Franco <franco_siete@yahoo.com>
2023-11-27 20:30:56 -0800

    Fixed text format

--- src/main/java/algorithms/HouseRobber.java
+++ src/main/java/algorithms/HouseRobber.java
@@ -5,21 +5,21 @@ import java.util.Random;
 /**
  * @author Francisco Franco
  *
- * You are a professional robber planning to rob houses 
- * along a street. Each house has a certain amount of money 
- * stashed, the only constraint stopping you from robbing 
- * each of them is that adjacent houses have security systems 
- * connected and it will automatically contact the police if 
- * two adjacent houses were broken into on the same night.
+ *         You are a professional robber planning to rob houses
+ *         along a street. Each house has a certain amount of money
+ *         stashed, the only constraint stopping you from robbing
+ *         each of them is that adjacent houses have security systems
+ *         connected and it will automatically contact the police if
+ *         two adjacent houses were broken into on the same night.
  *
- * Given an integer array nums representing the amount of money 
- * of each house, return the maximum amount of money you can 
- * rob tonight without alerting the police.
+ *         Given an integer array nums representing the amount of money
+ *         of each house, return the maximum amount of money you can
+ *         rob tonight without alerting the police.
  * 
  */
 
 public class HouseRobber {
-	
+
 	/**
 	 * @param tag
 	 * @param myArray
@@ -27,7 +27,7 @@ public class HouseRobber {
 	private static void printArray(String tag, int[] myArray) {
 		StringBuilder builder = new StringBuilder();
 		builder.append(tag + ": ");
-		
+
 		for (int l = 0; l < myArray.length; l++) {
 			builder.append(myArray[l] + " ");
 		}
@@ -44,16 +44,16 @@ public class HouseRobber {
 	 */
 	public static int robRecursive(int[] nums, int i) {
 		// DEBUG
-		//System.out.println("i" + i);
-    if (i < 0) {
+		// System.out.println("i" + i);
+		if (i < 0) {
 			return 0;
-    }
+		}
 		int max = Math.max(robRecursive(nums, i - 2) + nums[i], robRecursive(nums, i - 1));
-    // DEBUG
-		//System.out.println("max: " + max);
+		// DEBUG
+		// System.out.println("max: " + max);
 		return max;
 	}
-	
+
 	/**
 	 * Iterative + 2 variables -> Botttom-up approach
 	 * Time Complexity: O(n)
@@ -66,12 +66,11 @@ public class HouseRobber {
 		if (nums.length == 0) {
 			return 0;
 		}
-		
+
 		if (nums.length <= 2) {
 			if (nums.length == 1) {
 				return nums[0];
-			}
-			else {
+			} else {
 				return Math.max(nums[0], nums[1]);
 			}
 		}
@@ -85,22 +84,22 @@ public class HouseRobber {
 		}
 		return maxAmount;
 	}
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int[] numbers = {2, 7, 9, 3, 1};
-		//Random random = new Random();
-		//int[] numbers = new int[10];
-		//for (int i = 0; i < numbers.length; i++) {
-		//numbers[i] = random.nextInt(100);
-		//}
+		int[] numbers = { 2, 7, 9, 3, 1 };
+		// Random random = new Random();
+		// int[] numbers = new int[10];
+		// for (int i = 0; i < numbers.length; i++) {
+		// numbers[i] = random.nextInt(100);
+		// }
 		printArray("Orig", numbers);
-		
+
 		int maxRobAmount = robRecursive(numbers, numbers.length - 1);
 		System.out.println("Recursive maxRobAmount: " + maxRobAmount);
-		
+
 		maxRobAmount = robIterative(numbers);
 		System.out.println("Iterative maxRobAmount: " + maxRobAmount);
 	}
