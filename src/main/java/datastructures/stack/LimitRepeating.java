39a089877fa55ac9049ee584a279a739fa1e760a
Francisco Franco <franco_siete@yahoo.com>
2023-12-19 03:04:44 -0800

    Added header comments to describe problem and approach

--- src/main/java/datastructures/stack/LimitRepeating.java
+++ src/main/java/datastructures/stack/LimitRepeating.java
@@ -2,11 +2,25 @@
 /**
  * @author Francisco Franco
  *
+ * You are given a string value and a limit value to 
+ * limit the repeating characters in the output string.
+ *
+ * For example, given s = "AAAABBBCDEFAABBBBCCCCCC" 
+ * and limit = 3 return "AAABBBCDEFAABBBCCC".
+ * 
+ * Approach:
+ * Can use StringBuilder as stack or Stack<Character> data structure
+ * to store repeating characters up to the limit value. Iterate and  
+ * compare string characters at each index to last char in
+ * in the stack. If char count in stack is less than limit, then add
+ * the character to the stack. If string char and last char in stack
+ * do not match, then add new char to stack and reset count to 1.
  */
 
 import java.util.Stack;
 
 public class LimitRepeating {
+	public static final String INVALID_INPUT = "INVALID STRING INPUT";
 
 	public LimitRepeating() {}
 	
@@ -19,6 +33,7 @@ public class LimitRepeating {
 	 * @return
 	 */
 	public static String limitRepeating(String s, int limit, boolean doDebug) {
+		if (s == null || s.length() < 1) return INVALID_INPUT;
 		int bIndex = 0, count = 1;
 		StringBuilder builder = new StringBuilder();
 		builder.append(s.charAt(0));
@@ -54,6 +69,7 @@ public class LimitRepeating {
 	 * @return
 	 */
 	public static String limitRepeatingUsingStack(String s, int limit, boolean doDebug) {
+		if (s == null || s.length() < 1) return INVALID_INPUT;
 		int count = 1;
 		Stack<Character> stack = new Stack<>();
 		stack.push(s.charAt(0));
@@ -85,10 +101,13 @@ public class LimitRepeating {
 	// Driver code
 	public static void main(String args[]) {
 		String orig = "AAAABBBCDEFAABBBBCCCCCC";
-		System.out.println("orig: " + orig);
-		String output = limitRepeating(orig, 3, false); 
+		//String orig = "ABCDE";
+		//String orig = "";
+		int limit = 3;
+		System.out.println("orig: " + orig + " limit: " + limit);
+		String output = limitRepeating(orig, limit, false); 
 		System.out.println("output 1: " + output);
-		output = limitRepeatingUsingStack(orig, 3, false);
+		output = limitRepeatingUsingStack(orig, limit, false);
 		System.out.println("output 2: " + output);
 	}
 }
