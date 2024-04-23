c34fc593586154ef4463467ee89f6fe0742a41a9
Francisco Franco <franco_siete@yahoo.com>
2024-04-23 14:41:42 -0700

    Added module

---
+++ src/main/java/lambda/Java8PredicateTutorial.java
@@ -0,0 +1,22 @@
+import java.util.function.Predicate;
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Java8PredicateTutorial {
+	public static void main(String[] args) {
+		// Since input to predicate function is an integer
+		Predicate<Integer> lambdaFunc = x -> (x%2) == 0;
+		List<Integer> nums = Arrays.asList(99, 66, 88, 16);
+
+		// DEBUG
+		System.out.println("nums: " + nums);
+		
+		// Java predicate and lambda stream example usage
+		List<Integer> evenNums = nums.stream().filter(lambdaFunc).collect(Collectors.toList());
+		
+		// DEBUG
+		System.out.println("evenNums: " + evenNums);
+	
+	}
+}
